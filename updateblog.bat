645fdeacc2853a75e1ebc9ae09f542be48b4ba1f
LiangYu <3451196974@qq.com>
2025-09-17 22:22:44 +0800

    update 2025-09-17 2222

--- updateblog.bat
+++ updateblog.bat
@@ -1,4 +1,8 @@
 @echo off
+:: 1. 重新生成站点
+hugo
+
+:: 2. 把生成出来的文件一起交上去
 git add .
-git commit -m "UpdateBlog %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "update %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%%time:~3,2%"
 git push origin main
